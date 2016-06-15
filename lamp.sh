2285c0faa278a92683c3ed421358662796dacc23
Meryl Barantal <mba@octaveoctave.com>
2016-06-15 12:43:15 +0200

    chech if root

--- lamp.sh
+++ lamp.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-if netstat -laputen | grep '80$' || dpkg-query -l php5 || dpkg-query -l 'mysql*'
+if netstat -laputen | grep '80$' || dpkg-query -l php5 >&- 2>&- || dpkg-query -l 'mysql*' >&- 2>&-
 then
-  echo "Web server already installed! Do you wish to continue anyway?";
+  echo "Web server already installed! Do you wish to continue anyway?"
   read -p "Y/N" var install :
 else
-  apt-get update >&- 2>&-
-  apt-get upgrade >&- 2>&-
-  echo "Installing web dependancies..."
-  apt-get install -y vim sudo wget curl git zsh libxml2-dev libcurl3 mysql-client apache2 php5 php5-mysql php5-curl libapache2-mod-php5 php5-mcrypt php5-gd php5-cli php5-dev php5enmod mcrypt >&- 2>&-
-  debconf-set-selections <<< 'mysql-server mysql-server/root_password password 97ZE3Hj1Rrbn'
-  debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 97ZE3Hj1Rrbn'
-  apt-get -y install mysql-server >&- 2>&-
+  if [ "$UID" -ne "0" ]
+  then
+    echo -n "Update and upgrade..."
+    sudo apt-get -y update > /dev/null
+    echo -n "Update done..."
+    sudo apt-get -y upgrade > /dev/null
+    echo "All done"
+    echo "Installing web dependancies..."
+    sudo apt-get install -y vim sudo wget curl git zsh libxml2-dev libcurl3 mysql-client apache2 php5 php5-mysql php5-curl libapache2-mod-php5 php5-mcrypt php5-gd php5-cli php5-dev php5enmod mcrypt > /dev/null
+    debconf-set-selections <<< 'mysql-server mysql-server/root_password password 97ZE3Hj1Rrbn'
+    debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 97ZE3Hj1Rrbn'
+    sudo apt-get -y install mysql-server > /dev/null
+  else
+    echo -n "Update and upgrade..."
+    sudo apt-get -y update > /dev/null
+    echo -n "Update done..."
+    sudo apt-get -y upgrade > /dev/null
+    echo "All done"
+    echo "Installing web dependancies..."
+    apt-get install -y vim sudo wget curl git zsh libxml2-dev libcurl3 mysql-client apache2 php5 php5-mysql php5-curl libapache2-mod-php5 php5-mcrypt php5-gd php5-cli php5-dev php5enmod mcrypt > /dev/null
+    debconf-set-selections <<< 'mysql-server mysql-server/root_password password 97ZE3Hj1Rrbn'
+    debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 97ZE3Hj1Rrbn'
+    apt-get -y install mysql-server > /dev/null
+  fi
+
   echo "All web dependancies installed!"
   echo -e "\n"
 
