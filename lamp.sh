20e6c2ffbe995e8728061ed0ffeef643df25cbca
Meryl Barantal <mba@octaveoctave.com>
2016-06-15 12:21:39 +0200

    no verbose, add restart services

--- lamp.sh
+++ lamp.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
-if netstat -laputen | grep '80$'
+
+if netstat -laputen | grep '80$' || dpkg-query -l php5 || dpkg-query -l 'mysql*'
 then
   echo "Web server already installed! Do you wish to continue anyway?";
   read -p "Y/N" var install :
 else
-  apt-get update
-  apt-get upgrade
-  apt-get install -y vim sudo wget curl git zsh libxml2-dev libcurl3 mysql-server mysql-client apache2 php5 php5-mysql php5-curl
+  apt-get update >&- 2>&-
+  apt-get upgrade >&- 2>&-
+  echo "Installing web dependancies..."
+  apt-get install -y vim sudo wget curl git zsh libxml2-dev libcurl3 mysql-client apache2 php5 php5-mysql php5-curl libapache2-mod-php5 php5-mcrypt php5-gd php5-cli php5-dev php5enmod mcrypt >&- 2>&-
+  debconf-set-selections <<< 'mysql-server mysql-server/root_password password 97ZE3Hj1Rrbn'
+  debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 97ZE3Hj1Rrbn'
+  apt-get -y install mysql-server >&- 2>&-
+  echo "All web dependancies installed!"
+  echo -e "\n"
+
+  service apache2 restart && service mysql restart > /dev/null
+
+  echo -e "\n"
+
+  if [ $? -ne 0 ]; then
+    echo "Please Check the Install Services, There is some $(tput bold)$(tput setaf 1) Problem $(tput sgr0)"
+  else
+   echo "Installed Services run $(tput bold)$(tput setaf 2)Sucessfully$(tput sgr0)"
+  fi
+
+  echo -e "\n"
 fi
-
-# update repo && install softwares
-#apt-get update
-#apt-get upgrade
-
-# basics
-#apt-get install -y vim sudo wget curl git zsh libxml2-dev libcurl3
-
-# (l)amp
-#apt-get install mysql-server mysql-client apache2 php5 php5-mysql php5-curl
-
-#Environnement Nodejs
-#curl -sL https://deb.nodesource.com/setup_0.12 | bash -
-#apt-get install -y nodejs
-#curl -l https://npmjs.org/install.sh | sudo sh
-#npm install -g gulp
-#npm install -g grunt
-
-#Environnement ruby
-#apt-get install -y ruby-full
-#gem install sass
-#gem install compass
