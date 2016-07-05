05b54d4b2556c5e2e2830db67c13e13e382c8477
Meryl Barantal <meryl@MacBook-Pro-de-Meryl.local>
2016-07-05 14:20:39 +0200

    optimize it

--- lamp.sh
+++ lamp.sh
@@ -1,47 +1,41 @@
 #!/bin/bash
+no_print = >&- 2>&-
 
-if netstat -laputen | grep '80$' || dpkg-query -l php5 >&- 2>&- || dpkg-query -l 'mysql*' >&- 2>&-
+function install {
+  echo -n "Update and upgrade..."
+  apt-get -y update no_print
+  echo -n "Update done..."
+  apt-get -y upgrade no_print
+  echo "All done"
+  echo "Installing web dependancies..."
+  apt-get install -y vim sudo wget curl git zsh libxml2-dev libcurl3 mysql-client apache2 php5 php5-mysql php5-curl libapache2-mod-php5 php5-mcrypt php5-gd php5-cli php5-dev php5enmod mcrypt no_print
+  debconf-set-selections <<< 'mysql-server mysql-server/root_password password 97ZE3Hj1Rrbn'
+  debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 97ZE3Hj1Rrbn'
+  apt-get -y install mysql-server no_print
+}
+
+if netstat -laputen | grep '80$' || dpkg-query -l php5 >&- 2>&- || dpkg-query -l 'mysql*' no_print
 then
   echo "Web server already installed! Do you wish to continue anyway?"
-  read -p "Y/N" var install :
 else
   if [ "$UID" -ne "0" ]
-  then
-    echo -n "Update and upgrade..."
-    sudo apt-get -y update > /dev/null
-    echo -n "Update done..."
-    sudo apt-get -y upgrade > /dev/null
-    echo "All done"
-    echo "Installing web dependancies..."
-    sudo apt-get install -y vim sudo wget curl git zsh libxml2-dev libcurl3 mysql-client apache2 php5 php5-mysql php5-curl libapache2-mod-php5 php5-mcrypt php5-gd php5-cli php5-dev php5enmod mcrypt > /dev/null
-    debconf-set-selections <<< 'mysql-server mysql-server/root_password password 97ZE3Hj1Rrbn'
-    debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 97ZE3Hj1Rrbn'
-    sudo apt-get -y install mysql-server > /dev/null
+    echo "You are not sudoer! Try with sudo..."
   else
-    echo -n "Update and upgrade..."
-    sudo apt-get -y update > /dev/null
-    echo -n "Update done..."
-    sudo apt-get -y upgrade > /dev/null
-    echo "All done"
-    echo "Installing web dependancies..."
-    apt-get install -y vim sudo wget curl git zsh libxml2-dev libcurl3 mysql-client apache2 php5 php5-mysql php5-curl libapache2-mod-php5 php5-mcrypt php5-gd php5-cli php5-dev php5enmod mcrypt > /dev/null
-    debconf-set-selections <<< 'mysql-server mysql-server/root_password password 97ZE3Hj1Rrbn'
-    debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 97ZE3Hj1Rrbn'
-    apt-get -y install mysql-server > /dev/null
-  fi
+    install
 
-  echo "All web dependancies installed!"
-  echo -e "\n"
+    echo "All web dependancies installed!"
+    echo -e "\n"
 
-  service apache2 restart && service mysql restart > /dev/null
+    service apache2 restart && service mysql restart no_print
 
-  echo -e "\n"
+    echo -e "\n"
 
-  if [ $? -ne 0 ]; then
-    echo "Please Check the Install Services, There is some $(tput bold)$(tput setaf 1) Problem $(tput sgr0)"
-  else
-   echo "Installed Services run $(tput bold)$(tput setaf 2)Sucessfully$(tput sgr0)"
-  fi
+    if [ $? -ne 0 ]; then
+      echo "Please Check the Install Services, There is some $(tput bold)$(tput setaf 1) Problem $(tput sgr0)"
+    else
+     echo "Installed Services run $(tput bold)$(tput setaf 2)Sucessfully$(tput sgr0)"
+    fi
 
-  echo -e "\n"
+    echo -e "\n"
+  fi  
 fi
